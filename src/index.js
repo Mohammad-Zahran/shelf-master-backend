5208ca4c92f53060533985eaf6127ca449ab0dbc
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-18 00:09:20 +0200

    Established mongoose connection in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose"; 
 
 const app = express();
 
-dotenv.config()
+dotenv.config();
 
 app.use(express.json());
 app.use(
@@ -13,6 +14,15 @@ app.use(
     })
 );
 
+mongoose
+    .connect(
+        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@shelf-master-cluster.kfdar.mongodb.net/?retryWrites=true&w=majority&appName=shelf-master-cluster"`
+    )
+    .then(
+        console.log("MongoDB connected Succesfully!")
+    )
+    .catch((error) => console.log("Error connecting to MongoDB", error))
+
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Server running on port ${process.env.SERVER_PORT}`);
-})
+});
