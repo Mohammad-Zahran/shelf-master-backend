69793e92be72d9a8d07bfe264e0602830fd3ec8b
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-18 00:12:29 +0200

    Clean the code in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import mongoose from "mongoose"; 
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -9,20 +9,18 @@ dotenv.config();
 
 app.use(express.json());
 app.use(
-    cors({
-        origin: "*",
-    })
+  cors({
+    origin: "*",
+  })
 );
 
 mongoose
-    .connect(
-        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@shelf-master-cluster.kfdar.mongodb.net/?retryWrites=true&w=majority&appName=shelf-master-cluster"`
-    )
-    .then(
-        console.log("MongoDB connected Succesfully!")
-    )
-    .catch((error) => console.log("Error connecting to MongoDB", error))
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@shelf-master-cluster.kfdar.mongodb.net/?retryWrites=true&w=majority&appName=shelf-master-cluster"`
+  )
+  .then(console.log("MongoDB connected Succesfully!"))
+  .catch((error) => console.log("Error connecting to MongoDB", error));
 
 app.listen(process.env.SERVER_PORT, () => {
-    console.log(`Server running on port ${process.env.SERVER_PORT}`);
+  console.log(`Server running on port ${process.env.SERVER_PORT}`);
 });
