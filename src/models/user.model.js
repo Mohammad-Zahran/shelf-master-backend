24afc3d9a9fb5d3da8ae5bef2879c48a58dbb18f
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-18 00:28:19 +0200

    Add the wishlistSchema inside the User Model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,16 +1,29 @@
 // User Model:
 import mongoose from "mongoose";
 import { Schema } from "mongoose";
+
 // Cart Item Schema:
 const cartItemSchema = new Schema({
-    productId: String,
-    name: {
-        type: String,
-        trim: true,
-        required: true,
-        minlength: 3
-    },
-    image: String,
-    price: Number,
-    quantity: Number
-})
+  productId: String,
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+    minlength: 3,
+  },
+  image: String,
+  price: Number,
+  quantity: Number,
+});
+
+// Wishlist Item Schema
+const wishlistItemSchema = new Schema({
+  productId: String,
+  name: String,
+  image: String,
+  price: Number,
+  addedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
