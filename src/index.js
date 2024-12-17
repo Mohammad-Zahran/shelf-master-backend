6e690670ee031c8eb04f0e9f5afc77f0f5f980f7
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-17 02:36:57 +0200

    Setup dotenv for environment variable management

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+
+dotenv.config()
+
+app.use(express.json());
+app.use(
+    cors({
+        origin: "*",
+    })
+);
+
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`Server running on port ${process.env.SERVER_PORT}`);
+})
