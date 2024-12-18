f210e1ce133747f41249a33e4227e299a47243af
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-19 00:33:19 +0200

    Add the deleteProductItem API at the product.routes

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -3,12 +3,14 @@ import { Router } from "express";
 import {
     postProductItem,
     getAllProducItems,
+    deleteProductItem,
 } from "../controllers/product.controller.js";
 
 const router = new Router();
 
 router.post('/', postProductItem);
-router.get('/', getAllProducItems )
+router.get('/', getAllProducItems);
+router.delete('/:id', deleteProductItem);
 
 
 export default router;
