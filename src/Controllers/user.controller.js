0c19ed66cdaa863eb7cc1b2e4b8aa2235fa46c98
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-18 22:10:42 +0200

    Add getAllUsers API in user.controller

--- src/Controllers/user.controller.js
+++ src/Controllers/user.controller.js
@@ -15,3 +15,14 @@ export const register = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// get all users
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  }
+  catch (error) {
+    res.status(500).json({message: error.message});
+  }
+}
