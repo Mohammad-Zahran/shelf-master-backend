6bcdf671b9578e123da7bcfcf4b246f2316e5d85
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-18 23:36:38 +0200

    Add the route for the makeAdmin API

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,6 +7,7 @@ import {
   getAllUsers,
   deleteUser,
   getAdmin,
+  makeAdmin,
 } from "../controllers/user.controller.js";
 
 const router = new Router();
@@ -15,5 +16,6 @@ router.post("/", register);
 router.get("/", verifyToken, verifyAdmin, getAllUsers);
 router.delete("/:id", verifyToken, verifyAdmin, deleteUser);
 router.get('/admin/:email', verifyToken, getAdmin);
+router.patch('/admin/:id', verifyToken, verifyAdmin, makeAdmin);
 
 export default router;
