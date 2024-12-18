048edc611a87fab111c3806a99c129f397b9ed16
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-18 22:27:15 +0200

    Add the verification of the token

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -1,8 +1,17 @@
 import jwt from "jsonwebtoken";
 
-const verifyToken = (req, res, next) => {
-    if(!req.headers.authorization){
-        return res.status(401).send({message: "unauthorized access"});
-    }
+export const verifyToken = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
 
-}
+  const token = req.headers.authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "token is invalid!" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
