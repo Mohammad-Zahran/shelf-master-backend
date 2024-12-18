0fea6e0e110b82f43e155ba9dcf476d59e353a7d
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-18 23:35:43 +0200

    Add the makeAdmin API inside the user.controller

--- src/Controllers/user.controller.js
+++ src/Controllers/user.controller.js
@@ -59,3 +59,24 @@ export const getAdmin = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// make admin of a user
+export const makeAdmin = async (req, res) => {
+  const userId = req.params.id;
+  const {name, email, photoURL, role} = req.body;
+  try {
+      const updatedUser = await User.findByIdAndUpdate(
+          userId, 
+          {role: "admin"},
+          {new: true, runValidators: true}
+      );
+
+      if(!updatedUser){
+          return res.status(404).json({message: "User not found"})
+      }
+      res.status(200).json(updatedUser)
+      
+  } catch (error) {
+      res.status(500).json({ message: error.message });
+  }
+}
