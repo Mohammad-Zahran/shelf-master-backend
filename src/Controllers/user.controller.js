c0e1fe7604629153bb0a72a4fc98888e75cb1966
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-18 02:11:25 +0200

    Implement the register API with validation for existing emails

---
+++ src/Controllers/user.controller.js
@@ -0,0 +1,17 @@
+import { User } from "../models/user.model";
+
+// register
+export const register = async (req, res) => {
+  const user = req.body;
+  const query = { email: user.email };
+  try {
+    const existingUser = await User.findOne(query);
+    if (existingUser) {
+      return res.status(302).json({ message: "User already exists!" });
+    }
+    const result = await User.create(user);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
