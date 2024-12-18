fed0482b05182e4ae2bbb14c17fa4e4f446c81d9
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-18 22:52:30 +0200

    Update the user route to have the getAllUsers Route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,10 +4,13 @@ import { verifyAdmin } from "../middleware/verifyAdmin.js";
 
 import {
     register,
+    getAllUsers,
 } from "../controllers/user.controller.js";
 
 const router = new Router();
 
 router.post('/', register);
+router.get('/',verifyToken, verifyAdmin, getAllUsers);
+
 
 export default router;
