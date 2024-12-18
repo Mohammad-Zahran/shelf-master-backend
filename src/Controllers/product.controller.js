4e57fa1bd9ac04cb3f22b94b100c06fc9c77e482
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-19 00:20:12 +0200

    Add the get AllMenuItems API

--- src/Controllers/product.controller.js
+++ src/Controllers/product.controller.js
@@ -1,13 +1,22 @@
 import { Product } from "../models/product.model.js";
 
 // post a new product item
-export const postProductItem = async(req, res) => {
-    const newItem = req.body;
-    try {
-        const result = await Product.create(newItem);
-        res.status(200).json(result)
-    }
-    catch (error) {
-        res.status(500).json({message: error.message})
-    }
+export const postProductItem = async (req, res) => {
+  const newItem = req.body;
+  try {
+    const result = await Product.create(newItem);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// get all productItems
+export const getAllMenuItems = async (req, res) => {
+  try {
+    const products = await Product.find({}).sort({ createdAt: -1 });
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
