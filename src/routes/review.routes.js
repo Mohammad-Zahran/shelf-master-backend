9b87256f1513827ab3ab7ffb2fde7d25516e00ed
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-20 01:33:59 +0200

    Clean the routes files

--- src/routes/review.routes.js
+++ src/routes/review.routes.js
@@ -1,23 +1,21 @@
-import {Router} from "express";
+import { Router } from "express";
 
 import {
-    addReview,
-    getReviews,
-    updateReview,
-    deleteReview,
-    getAverageRating,
-    getReview
-} from "../controllers/review.controller.js"
+  addReview,
+  getReviews,
+  updateReview,
+  deleteReview,
+  getAverageRating,
+  getReview,
+} from "../controllers/review.controller.js";
 
 const router = new Router();
 
-router.post('/', addReview);
-router.get('/:id',getReviews);
-router.get('/:productId/:reviewId',getReview);
+router.post("/", addReview);
+router.get("/:id", getReviews);
+router.get("/:productId/:reviewId", getReview);
 router.get("/:id/average-rating", getAverageRating);
 router.patch("/:productId/:reviewId", updateReview);
 router.delete("/:productId/:reviewId", deleteReview);
-
-
 
 export default router;
