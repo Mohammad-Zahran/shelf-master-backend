6a8ef26122c6fa68a96253dfb12a43f49d772972
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-19 12:56:36 +0200

    Add the getCartByEmail API to the routes

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -5,10 +5,12 @@ import {
   deleteCart,
   updateCart,
   getSingleCart,
+  getCartByEmail,
 } from "./../controllers/cart.controller.js";
 
 const router = new Router();
 
+router.get('/', getCartByEmail);
 router.post("/", addToCart);
 router.delete("/:id", deleteCart);
 router.put("/:id", updateCart);
