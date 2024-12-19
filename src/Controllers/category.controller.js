faac6ec864971b11dfef918d810acb18af4365a1
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-20 01:31:33 +0200

    Add getCategoryById API

--- src/Controllers/category.controller.js
+++ src/Controllers/category.controller.js
@@ -3,7 +3,7 @@ import { Category } from "./../models/category.model.js";
 // Create a Category
 export const createCategory = async (req, res) => {
   try {
-    const category = await Category.create(req.body); 
+    const category = await Category.create(req.body);
     res
       .status(201)
       .json({ message: "Category created successfully!", category });
@@ -14,13 +14,26 @@ export const createCategory = async (req, res) => {
 
 // Get All Categories
 export const getCategories = async (req, res) => {
-    try {
-        const categories = await Category.find();
-        res.status(200).json(categories)
-    }
-    catch (error){
-        res.status(500).json({message: error.message});
-    }
-}
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
+// Get Single Category
+export const getCategoryById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const category = await Category.findById(id);
 
+    if (!category) {
+      return res.status(404).json({ message: "Category not found!" });
+    }
+
+    res.status(200).json(category);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 
 import {
-    createCategory,
-    getCategories,
-} from "../controllers/category.controller.js"
+  createCategory,
+  getCategories,
+  getCategoryById,
+} from "../controllers/category.controller.js";
 
 const router = new Router();
 
-router.post("/",createCategory);
-router.get("/",getCategories);
-
+router.post("/", createCategory);
+router.get("/", getCategories);
+router.get("/:id", getCategoryById);
 
 export default router;
