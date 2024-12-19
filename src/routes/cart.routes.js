31edd9309cf47e1bf18a73ca67c84811d24e84d3
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-19 12:03:39 +0200

    Add the updateCart in routes

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -3,12 +3,14 @@ import { Router } from "express";
 import {
     addToCart,
     deleteCart,
+    updateCart,
 } from './../controllers/cart.controller.js';
 
 const router = new Router();
 
 router.post("/", addToCart);
 router.delete('/:id', deleteCart)
+router.put('/:id', updateCart)
 
 
 export default router;
