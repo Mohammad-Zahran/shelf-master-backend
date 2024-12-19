736684e19170d7b91c7dafaefe9e4167c3f8061e
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-19 22:42:41 +0200

    Add the route of updateReviews

--- src/routes/review.routes.js
+++ src/routes/review.routes.js
@@ -3,11 +3,15 @@ import {Router} from "express";
 import {
     addReview,
     getReviews,
+    updateReview,
 } from "../controllers/review.controller.js"
 
 const router = new Router();
 
 router.post('/', addReview);
 router.get('/:id',getReviews);
+router.patch("/:productId/:reviewId", updateReview);
+
+
 
 export default router;
