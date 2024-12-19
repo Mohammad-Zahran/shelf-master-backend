691edf20929228d2ce080c53d25216edd14977b0
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-19 03:27:38 +0200

    Add the deleteCart API

--- src/Controllers/cart.controller.js
+++ src/Controllers/cart.controller.js
@@ -1,3 +1,4 @@
+import { FaCartShopping } from "react-icons/fa6";
 import { User } from "../models/user.model.js";
 
 // Add to Cart
@@ -11,7 +12,9 @@ export const addToCart = async (req, res) => {
       return res.status(404).json({ message: "User not found!" });
     }
 
-    const existingCartItem = user.cart.find(item => item.productId === productId);
+    const existingCartItem = user.cart.find(
+      (item) => item.productId === productId
+    );
 
     if (existingCartItem) {
       existingCartItem.quantity += quantity;
@@ -21,7 +24,23 @@ export const addToCart = async (req, res) => {
 
     await user.save();
 
-    res.status(200).json({ message: "Item added to cart successfully!", cart: user.cart });
+    res
+      .status(200)
+      .json({ message: "Item added to cart successfully!", cart: user.cart });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Delete Cart
+export const deleteCart = async (req, res) => {
+  const cartId = req.params.id;
+  try {
+    const deletedCart = await User.findByIdAndDelete(cartId);
+    if (!deletedCart) {
+      return res.status(401).json({ message: "Cart Items not found!" });
+    }
+    res.status(200).json({ message: "Cart Item deleted Succesfully!" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
