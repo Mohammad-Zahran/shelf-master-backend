dc30c5cac3b77a5ad449abbaf8a6cd58443fcd2e
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-20 01:25:25 +0200

    Create the getCategories API

--- src/Controllers/category.controller.js
+++ src/Controllers/category.controller.js
@@ -1,5 +1,6 @@
 import { Category } from "./../models/category.model.js";
 
+// Create a Category
 export const createCategory = async (req, res) => {
   try {
     const category = await Category.create(req.body); 
@@ -10,3 +11,16 @@ export const createCategory = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// Get All Categories
+export const getCategories = async (req, res) => {
+    try {
+        const categories = await Category.find();
+        res.status(200).json(categories)
+    }
+    catch (error){
+        res.status(500).json({message: error.message});
+    }
+}
+
+
--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -2,10 +2,13 @@ import { Router } from "express";
 
 import {
     createCategory,
+    getCategories,
 } from "../controllers/category.controller.js"
 
 const router = new Router();
 
 router.post("/",createCategory);
+router.get("/",getCategories);
+
 
 export default router;
