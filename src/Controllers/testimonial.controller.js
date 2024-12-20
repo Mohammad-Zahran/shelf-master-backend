9803ea54022cbc9f11f30cee33d7e2a2a446a973
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-20 23:28:07 +0200

    Add updateTestimonial API to update a testimonial info

--- src/Controllers/testimonial.controller.js
+++ src/Controllers/testimonial.controller.js
@@ -31,3 +31,30 @@ export const getAllTestimonials = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// Update a testimonial
+export const updateTestimonial = async (req, res) => {
+  const { email, testimonialId } = req.params;
+  const updatedTestimonial = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const testimonial = user.testimonials.id(testimonialId);
+
+    if (!testimonial) {
+      return res.status(404).json({ message: "Testimonial not found" });
+    }
+
+    Object.assign(testimonial, updatedTestimonial);
+    await user.save();
+
+    res.status(200).json({ message: "Testimonial updated successfully", user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- src/routes/testimonial.routes.js
+++ src/routes/testimonial.routes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import {
   addTestimonial,
-  getAllTestimonials
+  getAllTestimonials,
+  updateTestimonial,
 } from "../controllers/testimonial.controller.js";
 
 const router = new Router();
 
 router.post("/", addTestimonial); 
 router.get("/",getAllTestimonials);
+router.put("/:email/:testimonialId", updateTestimonial); 
 
 export default router;
