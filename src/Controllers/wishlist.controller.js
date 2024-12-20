58f0411a1ce14b2f95cafbd014042852ef9803c3
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-20 21:39:02 +0200

    Add the deleteWishList API

--- src/Controllers/wishlist.controller.js
+++ src/Controllers/wishlist.controller.js
@@ -36,3 +36,38 @@ export const getWishListItems = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// Remove an item from the wishlist based on productId and wishlistId
+export const removeWishlistItem = async (req, res) => {
+  const { email } = req.body;
+  const { id: wishlistId } = req.params;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found!" });
+    }
+
+    const wishListIndex = user.wishlist.findIndex(
+      (item) => item._id.toString() === wishlistId
+    );
+
+    if (wishListIndex === -1) {
+      return res.status(404).json({ message: "WishList item not found!" });
+    }
+
+    user.wishlist.splice(wishListIndex, 1);
+
+    await user.save();
+
+    res
+      .status(200)
+      .json({
+        message: "WishList item deleted successfully!",
+        wishlist: user.wishlist,
+      });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- src/routes/wishlist.routes.js
+++ src/routes/wishlist.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   addWishlistItem,
   getWishListItems,
+  removeWishlistItem,
 } from "../controllers/wishlist.controller.js";
 
 const router = new Router();
 
 router.post("/", addWishlistItem);
 router.get("/", getWishListItems);
+router.delete('/:id', removeWishlistItem);
 
 export default router;
