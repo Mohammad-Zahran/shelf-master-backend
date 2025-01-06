d7a8fded4fdeedd7a94a77dcc676efd29028e08c
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-01-07 01:46:12 +0200

    Add the logic to dind the count of the users, shelfItems, orders, revenue

--- src/routes/adminStats.routes.js
+++ src/routes/adminStats.routes.js
@@ -13,7 +13,31 @@ import { verifyAdmin } from "../middleware/verifyAdmin.js";
 const router = new Router();
 
 router.get('/', async(req, res) => {
-    res.send("Hello, Admin");
+    try {
+        const users = await User.countDocuments();
+        const shelfItems = await Product.countDocuments();
+        const orders = await Payment.countDocuments();
+
+        const result = await Payment.aggregate([{
+            $group: {
+                _id: null,
+                totalRevenue: {
+                    $sum: '$price'
+                }
+            }
+        }])
+        const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+
+        res.status(200).json({
+            users,
+            shelfItems,
+            orders,
+            revenue
+        })
+    }
+    catch (error){
+        res.status(500).send("Internal Server Error: " + error.message)
+    }
 });
 
 export default router;
