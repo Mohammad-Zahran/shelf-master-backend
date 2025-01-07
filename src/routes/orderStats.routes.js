8c4db1a24a6cdcf65323e783283f8b508bd8e76b
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-01-07 02:28:19 +0200

    Add the algorithim needed to get the orderStats

--- src/routes/orderStats.routes.js
+++ src/routes/orderStats.routes.js
@@ -8,12 +8,45 @@ import { Payment } from "../models/payment.model.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 import { verifyAdmin } from "../middleware/verifyAdmin.js";
 
-
 const router = new Router();
 
-router.get('/', async (req, res) => {
-    res.send("Order Status: ");
-})
-
+router.get("/", async (req, res) => {
+  try {
+    const result = await Payment.aggregate([
+      {
+        $unwind: "$productItems",
+      },
+      {
+        $lookup: {
+          from: "products",
+          localField: "productItems",
+          foreignField: "_id",
+          as: "productItemsDetails",
+        },
+      },
+      {
+        $unwind: "$productItemsDetails",
+      },
+      {
+        $group: {
+          _id: "$productItemsDetails.category",
+          quantity: { $sum: "$quantity" },
+          revenue: { $sum: "$price" },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          category: "$_id",
+          quantity: "$quantity",
+          revenue: "$revenue",
+        },
+      },
+    ]);
+    res.json(result);
+  } catch (error) {
+    res.statsu(500).send("Internal Server Error: " + error.message);
+  }
+});
 
 export default router;
