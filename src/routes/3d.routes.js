e1129c892a2e62c108bf20108bd3bfacc881bf7c
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-01-11 09:14:24 +0200

    Add the API route inside the 3d.routes.js

--- src/routes/3d.routes.js
+++ src/routes/3d.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { postModel } from "../Controllers/3d.controller.js";
+import { postModel, getAllModels } from "../Controllers/3d.controller.js";
 
 const router = new Router();
 
 router.post("/", postModel);
+router.get("/",getAllModels);
 
 export default router;
