066937fddb2e0ffbc0cd5ffbfa3a438ccae6d23a
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-01-15 04:30:54 +0200

    Update the adminStats.routes.js counts

--- src/routes/adminStats.routes.js
+++ src/routes/adminStats.routes.js
@@ -7,38 +7,48 @@ import { Payment } from "../models/payment.model.js";
 // middleware
 import { verifyToken } from "../middleware/verifyToken.js";
 import { verifyAdmin } from "../middleware/verifyAdmin.js";
+import { Model3D } from "../models/3d.model.js";
 
 // get all orders, users, payments, products item lengths
 
 const router = new Router();
 
-router.get('/', async(req, res) => {
-    try {
-        const users = await User.countDocuments();
-        const shelfItems = await Product.countDocuments();
-        const orders = await Payment.countDocuments();
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.countDocuments();
+    const shelfItems = await Product.countDocuments();
+    const orders = await Payment.countDocuments();
+    const models = await Model3D.countDocuments();
+    const testimonials = await User.aggregate([
+      { $unwind: "$testimonials" },
+      { $count: "totalTestimonials" },
+    ]);
 
-        const result = await Payment.aggregate([{
-            $group: {
-                _id: null,
-                totalRevenue: {
-                    $sum: '$price'
-                }
-            }
-        }])
-        const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+    const result = await Payment.aggregate([
+      {
+        $group: {
+          _id: null,
+          totalRevenue: {
+            $sum: "$price",
+          },
+        },
+      },
+    ]);
+    const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+    const testimonialCount =
+      testimonials.length > 0 ? testimonials[0].totalTestimonials : 0;
 
-        res.status(200).json({
-            users,
-            shelfItems,
-            orders,
-            revenue
-        })
-    }
-    catch (error){
-        res.status(500).send("Internal Server Error: " + error.message)
-    }
+    res.status(200).json({
+      users,
+      shelfItems,
+      orders,
+      revenue,
+      testimonialCount,
+      models,
+    });
+  } catch (error) {
+    res.status(500).send("Internal Server Error: " + error.message);
+  }
 });
 
 export default router;
-
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -19,6 +19,6 @@ router.get("/", verifyToken, verifyAdmin, getAllUsers);
 router.delete("/:id", verifyToken, verifyAdmin, deleteUser);
 router.get("/admin/:email", verifyToken, getAdmin);
 router.patch("/admin/:id", verifyToken, verifyAdmin, makeAdmin);
-router.put("/", updateProfile);
+router.put("/:id", updateProfile);
 
 export default router;
